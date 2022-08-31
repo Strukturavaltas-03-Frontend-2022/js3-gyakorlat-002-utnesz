0bd138cb5f345c8223d49328352f3c73bf43c403
utnesz <90829509+utnesz@users.noreply.github.com>
2022-08-31 11:00:05 +0200

    Ready

--- 09-storage/exercises/01/solution/js/setCookie.js
+++ 09-storage/exercises/01/solution/js/setCookie.js
@@ -1,15 +1,9 @@
-/*1. Írj egy `setCookie` nevű függvényt, ami a paraméterként kapott stringet eltárolja
-egy `token` nevű sütiben, ami 15 perc után lejár!*/
+'use strict'
 
-
-   const setCookie = (name, string, expirationDays = 15, path = '/') => {
-        const now = new Date();
-        now.setTime(now.getTime() + (expirationDays * 24 * 60 * 60 * 1000)),
-        const expires = now.toUTCString();
-        document.cookie = `${name}=${string};expires=${expires};path=${path}`;
-    };
-
-document.setCookie('token');
+const setCookie = (string) => {
+    const expires = new Date(Date.now() + 15 * 60 * 1000).toUTCString();
+    document.cookie = `token=${string};expires=${expires}`;
+};
 
 
 export default setCookie;
--- 09-storage/exercises/02/solution/js/cookieHandler.js
+++ 09-storage/exercises/02/solution/js/cookieHandler.js
@@ -1,44 +1,33 @@
-/*2. Készíts egy `cookieHandler` nevű objectet, az alábbi három metódust megvalósítja:
-- `getAll`: kiolvassa a sütik nevét és értékét, majd visszaadja őket egy objektumban, ahol a sütik neve a key és az értéke a value
-(pl. { viewed: 5 })
-- `toSessionStorage`: minden sütit egyenként elment a sessionStorage - ba az adott süti nevével és értékével
-- `flush`: törli az összes sütit
-
-A teszteléshez hozd létre a böngésződben az alábbi sütiket:
-- `viewed`: 5
-- `uid`: 354774631237
-- `ssid`: Bx55OWbHJ0Vt_IGIF*/
-
-
-
 const cookieHandler = {
-    setCookie(name, value, expirationDays = 365, path = '/') {
-        const now = new Date();
-        now.setTime(now.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
-        const expires = now.toUTCString();
-        document.cookie = `${name}=${value};expires=${expires};path=${path}`;
-    },
-    getAll(name) {
-        const keyValuePairs = document.cookie
-            .split('; ')
-            .find(cookie => cookie.startsWith(`${name}=`))
-        return keyValuePairs ? keyValuePairs.split('=')[1] : '';
-    },
-    checkCookie(name) {
-        const exists = cookieHandler.getCookie(name);
-        return exists ? true : false;
-    },
-    flush(name) {
-        document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-    }
-}
 
-cookieHandler.setCookie(`viewed`, '5');
-cookieHandler.setCookie(`uid`, '354774631237');
-cookieHandler.setCookie(`ssid`, 'Bx55OWbHJ0Vt_IGIF');
+    getAll() {
+        let cookiesObject = {};
+        const cookies = document.cookie.split("; ");
+        cookies.forEach((cookie) => {
+            const uniCookie = cookie.split("=");
+            cookiesObject[uniCookie[0]] = uniCookie[1];
+        });
+
+        return cookiesObject;
+    },
+
+    toSessionStorage() {
+        const cookiesObject = this.getAll();
+        Object.entries(cookiesObject).forEach(([key, value]) => {
+            sessionStorage.setItem(key, value);
+        });
+    },
+
+    flush() {
+        const cookiesObject = this.getAll();
+        Object.keys(cookiesObject).forEach((key) => {
+            document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+        });
+    },
+};
 
 
 cookieHandler.getAll();
 cookieHandler.flush();
 
-export default cookieHandler
+export default cookieHandler;
